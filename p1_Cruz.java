21fca91957fa9c03b6e1d7bae455c3693c97432b
cruza19 <cruza19@mail.sunysuffolk.edu>
2016-02-29 20:48:35 -0500

    Update p1_Cruz.java

--- p1_Cruz.java
+++ p1_Cruz.java
@@ -6,6 +6,7 @@ float nugX, nugY;
 float sunX, sunY;
 float xMin=200, yMin=200, dxMin=3, dyMin=2;
 float horizon;
+int score=1;
 
 
 //// SETUP: size, parameters for the horizon size and window size
@@ -31,18 +32,17 @@ void draw()
   nugshow();
   chasenug();
   msg();
-  
+  out();
 }
 
 /// SCENE: sky, grass, sun, house, Minion at (x,y)
 void scene()
 {
-  background( 150,120,255 );              // Sky Purple.
-  fill( 100,235,200 );                    // Mint
+  background( 150, 120, 255 );              // Sky Purple.
+  fill( 100, 235, 200 );                    // Mint
   noStroke();
   rectMode( CORNER );
-  rect( 0,horizon, width,height ); 
-  
+  rect( 0, horizon, width, height );
 }
 
 
@@ -51,54 +51,54 @@ void setting()
 /////House
 
 { 
-  fill( 250,100,0 );                // Orange
+  fill( 250, 100, 0 );                // Orange
   rectMode (CENTER);
-  rect( 335, horizon-40, 150,130,7 );       
+  rect( 335, horizon-40, 150, 130, 7 );       
   fill( 129, 3, 26);                // Maroon
-  triangle( 235,horizon-100, 435,horizon-100, 335,5 );
-/////Window 1
-  fill(255,255,255);                
-  rect(290, horizon-70, 40,40,7);
-/////Window 2
-  fill(255,255,255);                
-  rect(380, horizon-70, 40,40,7);
-/////Door
+  triangle( 235, horizon-100, 435, horizon-100, 335, 5 );
+  /////Window 1
+  fill(255, 255, 255);                
+  rect(290, horizon-70, 40, 40, 7);
+  /////Window 2
+  fill(255, 255, 255);                
+  rect(380, horizon-70, 40, 40, 7);
+  /////Door
   fill(0);                
-  rect(335, horizon-8, 55,65,7);
+  rect(335, horizon-8, 55, 65, 7);
 
 
-/////Trees
-  
+  /////Trees
+
   fill(103, 79, 65);                 //Trunk1
   rectMode (CENTER);
-  rect(500, horizon-30, 20,60);
-  
+  rect(500, horizon-30, 20, 60);
+
   fill(124, 209, 107);               //Leaf1
   ellipseMode(CENTER);
   ellipse(500, horizon-70, 50, 80); 
-  
-  
+
+
   fill(103, 79, 65);                 //Trunk3
   rectMode (CENTER);
-  rect(560, horizon-30, 20,60);
-  
+  rect(560, horizon-30, 20, 60);
+
   fill(104, 129, 57);               //Leaf3
   ellipseMode(CENTER);
   ellipse(560, horizon-70, 50, 80); 
-  
+
   fill(103, 79, 65);                 //Trunk2
   rectMode (CENTER);
-  rect(530, horizon-30, 20,60);
-  
+  rect(530, horizon-30, 20, 60);
+
   fill(184, 229, 157);               //Leaf2
   ellipseMode(CENTER);
   ellipse(530, horizon-70, 50, 80); 
-  
-  
+
+
   fill(103, 79, 65);                 //Trunk4
   rectMode (CENTER);
-  rect(100, horizon-30, 40,60);
-  
+  rect(100, horizon-30, 40, 60);
+
   fill(55, 216, 24);                  //Leaf4
   triangle(50, horizon-55, 100, horizon-125, 150, horizon-55);
   triangle(50, horizon-85, 100, horizon-140, 150, horizon-85);
@@ -117,15 +117,14 @@ void sunmove()                        ///sun move
   sunY=75;
   if (sunX > width) {
     sunX=  0;
-    
   }
   sunX=  sunX + 1;
 }
-                            //// Move Minion
+//// Move Minion
 void minmove()
 {
   //// Check boundaries.
-  
+
 
 
   //// Move Him
@@ -138,15 +137,14 @@ void sunshow()                            ///sun show
   fill(255, 177, 8);                      ///Yellow Sun
   ellipse( sunX, sunY, 90, 90 );
 }
-  
+
 void nugshow()                           ////Nugget show
 {
- 
+
+  stroke(0);
   ellipseMode(CENTER);
   fill(152, 118, 43);
-  ellipse(nugX, nugY, 60,30);
-
-
+  ellipse(nugX, nugY, 75, 65);
 }
 /// Minion
 void minshow()
@@ -154,11 +152,11 @@ void minshow()
   rectMode(CENTER);
   noStroke();
   fill(351, 255, 26);
-  rect( xMin,yMin, 75,100); //body
+  rect( xMin, yMin, 75, 100); //body
   arc( xMin, yMin-50, 75, 75, PI, TWO_PI); //head
-  
+
   eyes();
-    
+
   noFill();
   arc( xMin, yMin, 30, 30, HALF_PI, PI);
 }
@@ -176,31 +174,35 @@ void eyes()
 
 
 ////EVENTS
-void mousePressed()  {
+void mousePressed() {
   nugX= mouseX;
   nugY= mouseY;
 }
 
-void chasenug(){
-if ((xMin > nugX-10) || (yMin > nugY-10)) { 
-    nugX = random(0, 600);
-    nugY = random(0, 400);
-  
-  
-  
+void chasenug() {
+
+  if (dist(xMin, yMin, nugX, nugY) < 25) { 
+    nugX= random(10, 650);
+    nugY = random(10, 400);
+    score = score+1;
+  }
+}
+ void out(){
+   if (score==10 || key=='q') {
+     exit();
+   }
    
-
-}
-}
-
-
-
+ }
 /// MESSAGES
 void msg()
 {
+  textSize(22);
   fill(250, 250, 250);
-  text( "Proyect 1.", width/50,20 );
-  text( "=)", width/20,35 );
-  fill( 0,0,0 );
-  text( "Abel Cruz", 10,height-10 );
+  text( "Proyect 1.", width/50, 20 );
+  text( "=)", width/20, 45 );
+  fill( 0, 0, 0 );
+  text( "Abel Cruz", 10, height-10 );
+  textSize(20);
+  fill(250,50,0);
+  text(score, 650, horizon-220);
 }  
